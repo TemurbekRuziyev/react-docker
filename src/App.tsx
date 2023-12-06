f4ac403f038147348f0f433c11652668628e4c98
Temurbek Ruziev <temurbek.coder@gmail.com>
2023-12-07 00:03:46 +0500

    chore: initial commit

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h1>Temurbek Live Docker</h1>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
+  )
+}
+
+export default App
